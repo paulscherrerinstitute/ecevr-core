69804b8a5225793ce5765eaf4c9318f29e33cf96
till straumann <till.straumann@alumni.tu-berlin.de>
2022-08-18 10:29:41 +0200

    FoE2Spi.vhd: verify that page-to-be-written is blank

--- hdl/FoE2Spi.vhd
+++ hdl/FoE2Spi.vhd
@@ -60,10 +60,13 @@ architecture Impl of FoE2Spi is
    constant SPICMD_RD_STATUS_C    : std_logic_vector(7 downto 0) := x"05";
    constant SPICMD_WR_ENABLE_C    : std_logic_vector(7 downto 0) := x"06";
    constant SPICMD_WR_PAGE_C      : std_logic_vector(7 downto 0) := x"02";
+   constant SPICMD_READ_C         : std_logic_vector(7 downto 0) := x"03";
    constant SPICMD_ERASE_B64_C    : std_logic_vector(7 downto 0) := x"D8";
    constant SPICMD_ERASE_B32_C    : std_logic_vector(7 downto 0) := x"52";
    constant SPICMD_ERASE_S04_C    : std_logic_vector(7 downto 0) := x"20";
    constant SPICMD_ONES_C         : std_logic_vector(7 downto 0) := x"FF";
+
+   constant BLANK_C               : std_Logic_vector(7 downto 0) := x"FF";
 
    constant SPI_ST0_BUSY_IDX_C    : natural                      := 0;
 
@@ -92,14 +95,17 @@ architecture Impl of FoE2Spi is
    constant PRG_IDX_WREN_C        : natural := PRG_END_STATUS_C    + 1;
    constant PRG_END_WREN_C        : natural := PRG_IDX_WREN_C      + 2;
 
+   -- deassert CS, then issue a command
+   constant PRG_IDX_CSHI_CMD_C    : natural := PRG_END_WREN_C;
+
    constant PRG_IDX_CMD_C         : natural := PRG_END_WREN_C      + 1;
 
-   constant PRG_END_PAGE_WR_C     : natural := PRG_IDX_CMD_C       + 3;
+   constant PRG_END_ADDR_WR_C     : natural := PRG_IDX_CMD_C       + 3;
 
    constant NUM_SPI_MST_C         : natural := 2;
 
    constant PRG_TBL_C             : ProgArray := (
-      -- can use sequence of 3 to 
+      -- can use sequence of 3 to
       PRG_IDX_DEASS_CSB_C      =>
          ( CSB_NOT_ACT_C &  SPICMD_ONES_C      ),
       PRG_IDX_STATUS_C         =>
@@ -121,7 +127,17 @@ architecture Impl of FoE2Spi is
    subtype  ProgLenType is natural range  0 to PRGSZ_C     - 1;
    subtype  CounterType is natural range  0 to PAGE_SIZE_C - 1;
 
-   type StateType  is (IDLE, RUN_PROG, START_ERASE, ERASE, START_WRITE_PAGE, WRITE_PAGE, WAIT_PAGEWR, DONE);
+   subtype  CrcType     is std_logic_vector(31 downto 0);
+
+   constant CRC_POLY_C  : CrcType := x"04c11db7";
+
+   type StateType  is (
+      IDLE,
+      RUN_PROG,
+      START_ERASE, ERASE,
+      START_VERIFY_BLANK, VERIFY_BLANK,
+      START_WRITE_PAGE, WRITE_PAGE, WAIT_PAGEWR,
+      DONE);
 
    type RegType is record
       state       : StateType;
@@ -146,6 +162,12 @@ architecture Impl of FoE2Spi is
       erasing     : std_logic;
       writing     : std_logic;
       spiClaim    : std_logic;
+      crcX        : CrcType;
+      crcR        : CrcType;
+      misoLst     : std_logic;
+      sclkLst     : std_logic;
+      cselErr     : std_logic;
+      blnkErr     : std_logic;
    end record RegType;
 
    constant REG_INIT_C : RegType := (
@@ -169,7 +191,13 @@ architecture Impl of FoE2Spi is
       writeBlink  => (others => '0'),
       erasing     => '0',
       writing     => '0',
-      spiClaim    => '0'
+      spiClaim    => '0',
+      crcX        => (others => '0'),
+      crcR        => (others => '0'),
+      misoLst     => '0',
+      sclkLst     => '0',
+      cselErr     => '0',
+      blnkErr     => '0'
    );
 
    type SpiMuxStateType is ( IDLE, FOE, UDP );
@@ -203,7 +231,6 @@ architecture Impl of FoE2Spi is
       if ( deass ) then
          beg := PRG_IDX_DEASS_CSB_C;
       else
-         -- just shift ones
          beg := PRG_END_STATUS_C;
       end if;
       schedProg(v, beg, PRG_END_STATUS_C);
@@ -216,19 +243,23 @@ architecture Impl of FoE2Spi is
 
    procedure schedEraseBlock(variable v : inout RegType) is
    begin
-      schedProg(v, PRG_IDX_WREN_C, PRG_END_PAGE_WR_C, SPICMD_ERASE_BLK_F);
+      schedProg(v, PRG_IDX_WREN_C, PRG_END_ADDR_WR_C, SPICMD_ERASE_BLK_F);
    end procedure schedEraseBlock;
- 
+
    procedure schedWritePage(variable v : inout RegType) is
    begin
-      schedProg(v, PRG_IDX_WREN_C, PRG_END_PAGE_WR_C, SPICMD_WR_PAGE_C);
+      schedProg(v, PRG_IDX_WREN_C, PRG_END_ADDR_WR_C, SPICMD_WR_PAGE_C);
    end procedure schedWritePage;
-  
+
+   procedure schedRead(variable v : inout RegType) is
+   begin
+      schedProg(v, PRG_IDX_CSHI_CMD_C, PRG_END_ADDR_WR_C, SPICMD_READ_C);
+   end procedure schedRead;
+
    function sameBlk(constant a1, a2: A24Type) return boolean is
    begin
       return a1(a1'left downto LD_ERASE_BLK_SIZE_G) = a2(a2'left downto LD_ERASE_BLK_SIZE_G);
    end function sameBlk;
-
 
    signal r               : RegType := REG_INIT_C;
    signal rin             : RegType;
@@ -265,6 +296,10 @@ architecture Impl of FoE2Spi is
    signal foeSubLoc       : FoeSubType;
    signal busRepLoc       : Udp2BusRepType := UDP2BUSREP_INIT_C;
 
+   signal sclkLoc         : std_logic;
+   signal mosiLoc         : std_logic;
+   signal scsbLoc         : std_logic;
+
 begin
 
    G_CHECK : for i in FILE_MAP_G'range generate
@@ -284,8 +319,10 @@ begin
           or ( EN_SIM_G                 )
           report "invalid flash block size" severity failure;
 
-   debug(3 downto 0)         <= std_logic_vector( resize( to_signed( r.idx, 5 ), 4 ) );
-   debug(6 downto 4)         <= toSlv( StateType'pos( r.state ), 3 );
+   debug(3 downto 0)         <= toSlv( StateType'pos( r.state ), 4 );
+   debug(4)                  <= r.cselErr;
+   debug(5)                  <= r.blnkErr;
+   debug(6)                  <= '0';
    debug(7)                  <= r.foeSub.done;
    debug(8)                  <= foeMst.doneAck;
    debug(9)                  <= foeSubLoc.strmRdy;
@@ -306,22 +343,20 @@ begin
    debug(38)                 <= spiClaimFoE;
    debug(39)                 <= spiGrantFoE;
    debug(41 downto 40)       <= toSlv( SpiMuxStateType'pos( spiMuxState ), 2 );
-   debug(63 downto 42)       <= (others => '0');
+   debug(51 downto 42)       <= std_logic_vector( r.addr(9 downto 0) );
+   debug(55 downto 52)       <= std_logic_vector( resize( to_signed( r.idx, 5 ), 4 ) );
+   debug(63 downto 56)       <= (others => '0');
 
-   P_COMB : process ( r, wrRdyFoE, rdVldFoE, rdDat, foeMst, spiGrantFoE ) is
+   P_COMB : process ( r, wrRdyFoE, rdVldFoE, rdDat, foeMst, spiGrantFoE, sclkLoc, mosiLoc, miso, scsbLoc ) is
       variable v : RegType;
    begin
       v                  := r;
 
       foeSubLoc          <= r.foeSub;
-      wrVldFoE           <= '0';
-      rdRdyFoE           <= '0';
+      wrVldFoE           <= r.wrVld;
+      rdRdyFoE           <= r.rdRdy;
       foeSubLoc.strmRdy  <= '0';
       csbFoE             <= r.csb;
-
-      if ( (r.wrVld and wrRdyFoE) = '1' ) then
-         v.wrVld := '0';
-      end if;
 
       if    ( r.idx < 0                 ) then
          wrDatFoE <= foeMst.strmMst.data(7 downto 0);
@@ -338,6 +373,34 @@ begin
          wrDatFoE <= std_logic_vector( r.addr( 7 downto  0) );
       else
          wrDatFoE <= r.cmd;
+      end if;
+
+      -- keep track of sclk transitions
+      v.sclkLst := sclkLoc;
+      -- because SCLK and MOSI are generated locally and synchronous
+      -- to 'clk': when we see a transition (sclkLoc and not r.sclkLst)
+      -- it was actually produced by the previous 'clk' cycle which is
+      -- when we registered miso in the bit shifter.
+      -- Keep a delayed version of miso around...
+      v.misoLst := miso;
+
+      -- compute CRCs at positive SCLK transition
+      if ( ( sclkLoc and not r.sclkLst ) = '1' ) then
+         -- CSEL must be asserted
+         if ( scsbLoc = CSB_NOT_ACT_C ) then
+            v.cselErr := '1';
+         else
+            if ( r.state = WRITE_PAGE ) then
+               v.crcX := r.crcX( r.crcX'left - 1 downto 0 ) & '0';
+               if ( ( r.crcX(r.crcX'left) xor mosiLoc ) = '1' ) then
+                  v.crcX := v.crcX xor CRC_POLY_C;
+               end if;
+            end if;
+            v.crcR := r.crcR( r.crcR'left - 1 downto 0 ) & '0';
+            if ( ( r.crcR(r.crcR'left) xor r.misoLst ) = '1' ) then
+               v.crcR := v.crcR xor CRC_POLY_C;
+            end if;
+         end if;
       end if;
 
       case r.state is
@@ -360,8 +423,6 @@ begin
 
          -- set csb, cmd, idx and len before entry
          when RUN_PROG =>
-            wrVldFoE <= r.wrVld;
-            rdRdyFoE <= r.rdRdy;
             if (  (r.rdRdy or r.wrVld)  = '0' ) then
                v.wrVld := '1';
             elsif ( (r.wrVld and wrRdyFoE) = '1' ) then
@@ -398,13 +459,47 @@ begin
                      v.retState    := DONE;
                   elsif ( sameBlk( r.addr, r.laddr ) ) then
                      v.addr        := FILE_MAP_G( foeMst.fileIdx ).begAddr;
-                     v.state       := START_WRITE_PAGE;
+                     v.state       := START_VERIFY_BLANK;
                      v.eraseBlink  := (others => '0');
                   else
                      v.addr        := r.addr + BLK_SIZE_C;
                      v.state       := START_ERASE;
                      v.eraseBlink  := r.eraseBlink + 1;
                   end if;
+               end if;
+            end if;
+
+         when START_VERIFY_BLANK =>
+            if ( not r.progDone ) then
+               schedRead( v );
+            else
+               v.progDone := false;
+               v.cnt      := PAGE_SIZE_C - 1;
+               v.state    := VERIFY_BLANK;
+               v.idx      := PRG_IDX_CMD_C;
+               v.cmd      := SPICMD_ONES_C;
+            end if;
+
+         when VERIFY_BLANK =>
+            if (  (r.rdRdy or r.wrVld)  = '0' ) then
+               v.wrVld := '1';
+            elsif ( (r.wrVld and wrRdyFoE) = '1' ) then
+               v.wrVld := '0';
+               v.rdRdy := '1';
+            elsif ( (r.rdRdy and rdVldFoE) = '1' ) then
+               v.rdRdy := '0';
+               v.rdDat := rdDat;
+               -- there should be no need to limit to the max. address
+               -- since we should have erased the entire block containing
+               -- the start address
+               if ( rdDat /= BLANK_C ) then
+                  v.blnkErr  := '1';
+                  schedDeactCS( v );
+                  v.retState := DONE;
+               elsif ( r.cnt = 0 ) then
+                  v.state := START_WRITE_PAGE;
+               else
+                  v.cnt   := r.cnt - 1;
                end if;
             end if;
 
@@ -418,11 +513,12 @@ begin
                v.cnt      := PAGE_SIZE_C - 1;
                v.idx      := -1;
                v.state    := WRITE_PAGE;
+               -- start CRC computation
+               v.crcX     := (others => '1');
             end if;
 
          -- assume CSB is asserted already
          when WRITE_PAGE =>
-            rdRdyFoE <= r.rdRdy;
             if ( r.rdRdy = '0' ) then
                if ( foeMst.err = '1' ) then
                   schedDeactCS( v );
@@ -470,7 +566,7 @@ begin
                      schedDeactCS( v );
                      v.retState    := DONE;
                   else
-                     v.state       := START_WRITE_PAGE;
+                     v.state       := START_VERIFY_BLANK;
                      v.writeBlink  := r.writeBlink + 1;
                   end if;
                end if;
@@ -482,7 +578,11 @@ begin
             v.spiClaim   := '0';
             v.eraseBlink := (others => '0');
             v.writeBlink := (others => '0');
-            if ( (not r.foeSub.done and not foeMst.fifoRst) = '1' ) then
+            if ( (r.cselErr or r.blnkErr) = '1' ) then
+               v.foeSub.abort := '1';
+               v.cselErr      := '0';
+               v.blnkErr      := '0';
+            elsif ( (not r.foeSub.done and not foeMst.fifoRst) = '1' ) then
                v.foeSub.done  := '1';
             elsif ( (r.foeSub.done and foeMst.doneAck) = '1' ) then
                v.foeSub.done  := '0';
@@ -548,10 +648,10 @@ begin
          vldOut  => rdVld,
          rdyOut  => rdRdy,
 
-         serClk  => sclk,
+         serClk  => sclkLoc,
          serInp  => miso,
-         serOut  => mosi,
-         serCsb  => scsb
+         serOut  => mosiLoc,
+         serCsb  => scsbLoc
       );
 
    P_SPI_MUX : process (
@@ -611,7 +711,7 @@ begin
          rdVldBus    <= rdVld;
          rdRdy       <= rdRdyBus;
       end if;
-      
+
       spiMuxStateIn <= v;
    end process P_SPI_MUX;
 
@@ -620,6 +720,9 @@ begin
    foeSub      <= foeSubLoc;
    spiRst      <= rst;
    busRep      <= busRepLoc;
+   sclk        <= sclkLoc;
+   mosi        <= mosiLoc;
+   scsb        <= scsbLoc;
 
    progress(3) <= r.writing;
    progress(2) <= r.erasing;
